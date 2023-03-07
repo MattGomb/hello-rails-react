f4b26b4e7283516f81d798d27e4d34e5339c40b0
MattGomb <gombosonline@gmail.com>
2023-03-07 09:28:26 -0500

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :messages
   root "root#index"
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
 end
