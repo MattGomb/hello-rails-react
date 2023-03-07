8d29c0f283ea4c1dd5983d08a033f9a34c6cbe07
MattGomb <gombosonline@gmail.com>
2023-03-07 11:12:14 -0500

    Fix routing

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,17 @@
+
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchGreetings } from "../redux/greetings/GreetingsSlice";
+
+function Greeting() {
+  const greeting = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, []);
+
+  return <div>{greeting.msg}</div>;
+}
+
+export default Greeting;
